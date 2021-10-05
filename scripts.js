e45ed474675e25fec275c49ba074b0f8bd821e12
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-05 18:54:11 -0300

    innerHTMLTransaction do elemento trr

--- scripts.js
+++ scripts.js
@@ -1,18 +1,66 @@
-let Modal = {
+const Modal = {
     open(){
-        //abrir o modal
-        //adicionar a class active ao modal 
         document
             .querySelector('.modal-overlay')
             .classList
             .add('active')
     },
     close(){
-        //fechar o modal
-        //remover a class active ao modal
         document
             .querySelector('.modal-overlay')
             .classList
             .remove('active')
     }
 }
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amout: -50000,
+        date: 01/10/2021,
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amout: 100000,
+        date: 10/10/2021,
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amout: 12000,
+        date: 03/10/2021,
+    },
+]
+
+const Transaction = {
+    incomes(){
+
+    },
+    expenses(){
+
+    },
+    total(){
+
+    },
+}
+
+const DOM = {
+    addTransaction(transaction, index){
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction()
+        console.log(tr.innerHTML)
+    },
+    innerHTMLTransaction(){
+        const html = ` 
+            <td class="description">${transaction.description}</td>
+            <td class="expense">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td> 
+            <td><img src="./assets/minus.svg" alt="Remover transação."></td> 
+        `
+        return html
+    }
+}
+
+DOM.addTransaction(transactions[0])
