b3bc7905fe18146dff6fc62c1874fd123c7fe0c4
Douglas A B Novato <douglasabnovato@gmail.com>
2021-10-06 12:59:52 -0300

    preenchendo as transações via js

--- scripts.js
+++ scripts.js
@@ -13,13 +13,13 @@ const Modal = {
 
 const Transaction = {
     incomes(){
-
+        //somar as entradas
     },
     expenses(){
-
+        //somar as saídas
     },
     total(){
-
+        //remover das entradas o valor das saídas
     },
 }
 
@@ -27,39 +27,37 @@ const transactions = [
     {
         id: 1,
         description: 'Cemig',
-        amout: -50000,
-        date: 01/10/2021,
+        amount: -50000,
+        date: '01/10/2021',
     },
     {
         id: 2,
         description: 'Website Top',
-        amout: 100000,
-        date: 10/10/2021,
+        amount: 100000,
+        date: '10/10/2021',
     },
     {
         id: 3,
         description: 'Internet Vero',
-        amout: 12000,
-        date: 03/10/2021,
+        amount: 12000,
+        date: '03/10/2021',
     },
 ]
-
-
 
 const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
     addTransaction(transaction, index){
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction() 
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction) 
         DOM.transactionsContainer.appendChild(tr)
     },
-    innerHTMLTransaction(){
-        const html = ` 
+    innerHTMLTransaction(transaction) {
+        const html = `
             <td class="description">${transaction.description}</td>
             <td class="expense">${transaction.amount}</td>
             <td class="date">${transaction.date}</td> 
-            <td><img src="./assets/minus.svg" alt="Remover transação."></td> 
-        `
+            <td><img src="./assets/minus.svg" alt="Remover transação."></td>
+        ` 
         return html
     }
 }
